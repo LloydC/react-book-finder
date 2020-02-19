26d85081fa321621da00880f4925a0658a61847f
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-02-19 11:48:10 +0100

    first commit - home setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Book Finder</h1>
+        
+        <form action="">
+        <input type="text" placeholder="Enter book title here ..."/>
+        <button class="myButton" type="submit">Search</button>
+      </form>
       </header>
+     
     </div>
   );
 }
