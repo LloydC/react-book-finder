31e55e4f391a0c21ced876e0a66f680a4c383509
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-03-26 16:34:07 +0000

    Added some more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
+    
   });
+  test('page title renders', () => {
+    render(<App />);
+    expect(screen.getByText('Book Finder')).toBeInTheDocument();
+  })
+  test('Search input renders', () => {
+    render(<App />);
+    expect(screen.getByPlaceholderText('Enter a book title here ...')).toBeInTheDocument();
+  })
+  test('Submit button renders', () => {
+    render(<App />);
+    expect(screen.getByRole('button'));
+  })
 })
 
