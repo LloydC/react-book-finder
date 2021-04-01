95e4aad93638820b47769064a9aec20cfcc9c083
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-04-01 13:48:21 +0000

    Removed react-collapsible && Added conditional rendering for book description

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Rating from 'react-verdict';
-import Collapsible from 'react-collapsible';
-import './Book.css'
+import './Book.css';
 
 export default function Book(props){
+    const [readMore, setReadMore]= useState(false);
     const {title, description, authors, imageLinks, publishedDate, averageRating, infoLink} = props.book;
 
     return (
         <div className="Book">
-            <h2 className="important">
-                <span role="img" aria-label="books">&#128215;</span>{title} <span role="img" aria-label="books">&#128215;</span>
+            <h2 className="book-title">
+                <span role="img" aria-label="books">&#128215;</span>
+                {title} 
+                <span role="img" aria-label="books">&#128215;</span>
             </h2>
             <a href={infoLink} target="_new">
                {imageLinks ? <img src={imageLinks.smallThumbnail ? imageLinks.smallThumbnail : imageLinks.thumbnail} alt='book_preview' className="highlight"/> : ''} 
             </a>
-            {description ? <Collapsible trigger="View summary"><span>{description}</span></Collapsible> : ''}
+            {description && !readMore ? <span onClick={()=> setReadMore(true)}>View summary</span> : ''}
+            {description && readMore ? <span onClick={()=> setReadMore(false)}>{description}</span> : ''}
             {averageRating ? <div style={{display:'flex', justifyContent: 'center'}}><Rating value={averageRating} /></div> : <p>No ratings available</p>}
             <div className="info">
             {publishedDate ? <p style={{width: '100%', textAlign: 'center'}}><span className="important">Released date</span> {publishedDate} </p> : ''}
